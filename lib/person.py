32f857cf24d08c1f7daa9e49ec28c1626249c102
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-08-30 06:22:48 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+     def talk(self):
+          print("Hello World!")
+     def walk(self):
+          print("The person is walking.")
+
+mark = Person()
+mark.talk()
+mark.walk()
+
+ 
+
